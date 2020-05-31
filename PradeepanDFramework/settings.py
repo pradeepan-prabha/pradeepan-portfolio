b201456193d6ac852543a18997282535fde4623d
Pradeepan <pradeepan.prabha@gmail.com>
2020-06-01 03:50:57 +0530

    Prod: Pradeepan Profolio Producation v0.0.1

--- PradeepanDFramework/settings.py
+++ PradeepanDFramework/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # Simplified static file serving.
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -114,85 +117,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-BOOTSTRAP4 = {
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-    # The complete URL to the Bootstrap CSS file
-    # Note that a URL can be either a string,
-    # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
-    # or a dict like the default value below.
-    "css_url": {
-        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
-        "integrity": "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
-        "crossorigin": "anonymous",
-    },
-
-    # The complete URL to the Bootstrap JavaScript file
-    "javascript_url": {
-        "url": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
-        "integrity": "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T",
-        "crossorigin": "anonymous",
-    },
-
-    # The complete URL to the Bootstrap CSS file (None means no theme)
-    "theme_url": None,
-
-    # The URL to the jQuery JavaScript file (full)
-    "jquery_url": {
-        "url": "https://code.jquery.com/jquery-3.3.1.min.js",
-        "integrity": "sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT",
-        "crossorigin": "anonymous",
-    },
-
-    # The URL to the jQuery JavaScript file (slim)
-    "jquery_slim_url": {
-        "url": "https://code.jquery.com/jquery-3.3.1.slim.min.js",
-        "integrity": "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
-        "crossorigin": "anonymous",
-    },
-
-    # The URL to the Popper.js JavaScript file (slim)
-    "popper_url": {
-        "url": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
-        "integrity": "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49",
-        "crossorigin": "anonymous",
-    },
-
-    # Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap4.html)
-    'javascript_in_head': False,
-
-    # Include jQuery with Bootstrap JavaScript False|falsy|slim|full (default=False)
-    # False - means tag bootstrap_javascript use default value - `falsy` and does not include jQuery)
-    'include_jquery': False,
-
-    # Label class to use in horizontal forms
-    'horizontal_label_class': 'col-md-3',
-
-    # Field class to use in horizontal forms
-    'horizontal_field_class': 'col-md-9',
-
-    # Set placeholder attributes to label if no placeholder is provided
-    'set_placeholder': True,
-
-    # Class to indicate required (better to set this in your Django form)
-    'required_css_class': '',
-
-    # Class to indicate error (better to set this in your Django form)
-    'error_css_class': 'is-invalid',
-
-    # Class to indicate success, meaning the field has valid input (better to set this in your Django form)
-    'success_css_class': 'is-valid',
-
-    # Renderers (only set these if you have studied the source and understand the inner workings)
-    'formset_renderers':{
-        'default': 'bootstrap4.renderers.FormsetRenderer',
-    },
-    'form_renderers': {
-        'default': 'bootstrap4.renderers.FormRenderer',
-    },
-    'field_renderers': {
-        'default': 'bootstrap4.renderers.FieldRenderer',
-        'inline': 'bootstrap4.renderers.InlineFieldRenderer',
-    },
-}
+# Activate Django-Heroku.
+django_heroku.settings(locals())
