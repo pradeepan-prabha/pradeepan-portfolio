38b35842e01f9243fd6b3ad82f643bffc4ab3b23
Pradeepan <pradeepan.prabha@gmail.com>
2020-06-01 18:16:10 +0530

    Prod: PradeepanDFramework url modifiy to direct access to portfolio and /pradeepan  v0.0.6

--- PradeepanDFramework/urls.py
+++ PradeepanDFramework/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('pradeepan/', include('pradeepan_portfolio.urls')),
     path('', include('pradeepan_portfolio.urls')),
     path('admin/', admin.site.urls),
 ]
